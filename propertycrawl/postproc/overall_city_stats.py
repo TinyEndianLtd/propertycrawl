89704dc9813ec9107b2e3c65603259b6d97bfb47
Karol Majta <karol@tinyendian.com>
2018-04-18 19:46:51 +0200

    Added aditional cleaning of stats for blank results

--- propertycrawl/postproc/overall_city_stats.py
+++ propertycrawl/postproc/overall_city_stats.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     all_districts = get_districts(all_property) 
     add_districts(all_property, all_districts)
 
-    results = [(u"City Average", avg_roi(for_rent, for_sale) * 100)]
+    results = []
     for district in all_districts:
         for_rent_in_district = [datum for datum in for_rent if datum['district'] == district]
         for_sale_in_district = [datum for datum in for_sale if datum['district'] == district]
@@ -31,6 +31,7 @@ if __name__ == '__main__':
         else:
             results.append((district, ""))
 
-    for r in sorted(results, key=lambda p: p[1], reverse=True):
+    clean_results = [r for r in results if r[1]]
+    for r in sorted(clean_results, key=lambda p: p[1], reverse=True):
         datum = {'district': r[0], 'roi': r[1]}
         sys.stdout.buffer.write(u"{0}{1}".format(json.dumps(datum), os.linesep).encode('utf-8'))
