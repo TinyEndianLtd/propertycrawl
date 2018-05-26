bf10aebaa73c3464fa73a12578aa5cf788637beb
Karol Majta <karol@tinyendian.com>
2018-05-26 12:57:32 +0200

    Disabled proxies and fine tuned crawl params for faster crawls

--- propertycrawl/settings.py
+++ propertycrawl/settings.py
@@ -32,18 +32,18 @@ ROTATING_PROXY_LIST_PATH = 'proxies.txt'
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 1,
-    'rotating_proxies.middlewares.RotatingProxyMiddleware': 2,
-    'rotating_proxies.middlewares.BanDetectionMiddleware': 3,
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 4,
+    #    'rotating_proxies.middlewares.RotatingProxyMiddleware': 2,
+    #    'rotating_proxies.middlewares.BanDetectionMiddleware': 3,
+    #    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 4,
 }
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 64
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
